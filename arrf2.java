a915f53a5a5246e6c78c2f334203847bcd8b7770
Kumkum Kaslikar <108987698+Kumkum1012kaslikar@users.noreply.github.com>
2024-01-16 18:20:51 +0530

    Add files via upload

---
+++ arrf2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class arrf2 {
+    public static int[] takeInput() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        return arr;
+    }
+
+    public static int largest(int arr[]) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        int lar = largest(arr);
+        System.out.println(lar);
+    }
+}
