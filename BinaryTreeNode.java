424abadb83372711a2bcfefde096be0eaab6e13a
Kumkum Kaslikar <108987698+Kumkum1012kaslikar@users.noreply.github.com>
2024-02-01 02:52:41 +0530

    Add files via upload

---
+++ BinaryTreeNode.java
@@ -0,0 +1,12 @@
+package Binary;
+
+public class BinaryTreeNode<T> {
+
+    T data;
+    public BinaryTreeNode<T> left;
+    public BinaryTreeNode<T> right;
+
+    public BinaryTreeNode(T data) {
+        this.data = data;
+    }
+}
