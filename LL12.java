6a6e4fc11fe67a15407df5031b1eb997ade18083
Kumkum Kaslikar <108987698+Kumkum1012kaslikar@users.noreply.github.com>
2024-02-07 20:59:30 +0530

    Add files via upload

---
+++ LL12.java
@@ -0,0 +1,26 @@
+package LinkedList;
+
+import java.util.ArrayList;
+
+public class LL12 {
+    public static ArrayList<Integer> removeConsecutiveDuplicates(int arr[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(arr[0]);
+        for (int i = 1; i <= arr.length - 1; i++) {
+            if (arr[i] != arr[i - 1]) {
+                result.add(arr[i]);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 10, 20, 20, 20, 30, 40, 10 };
+        System.out.println(removeConsecutiveDuplicates(arr));
+        ArrayList<Integer> result = removeConsecutiveDuplicates(arr);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+
+    }
+}
