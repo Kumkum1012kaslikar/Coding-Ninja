9d01fb2cd06c3a4f09a1b10e04f4e7b3d312635d
Kumkum Kaslikar <108987698+Kumkum1012kaslikar@users.noreply.github.com>
2024-02-11 08:22:55 +0530

    Add files via upload

---
+++ pT2.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class pT2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j >= 1; j--) {
+
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("for while loop:");
+        int i = 1;
+        while (i <= n) {
+            int j = i;
+            while (j >= 1) {
+                System.out.print(j);
+                j--;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
