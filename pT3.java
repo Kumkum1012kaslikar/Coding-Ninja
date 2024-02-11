6fb14e62732c67a297e0b7807810cf6770f755c6
Kumkum Kaslikar <108987698+Kumkum1012kaslikar@users.noreply.github.com>
2024-02-11 08:23:20 +0530

    Add files via upload

---
+++ pT3.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class pT3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+        System.out.println("for while loop:");
+        int i = 1;
+        while (i <= n) {
+            int j = 1;
+            while (j <= i) {
+                System.out.print(j);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
