284bf7fb92cd2d2075eb6477a816a740be8fcb5f
Kumkum Kaslikar <108987698+Kumkum1012kaslikar@users.noreply.github.com>
2024-02-17 09:40:14 +0530

    Add files via upload

---
+++ pTR6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class pTR6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        for (i = 1; i <= n;) {
+
+            System.out.println("");
+            i = i + 1;
+        }
+    }
+}
